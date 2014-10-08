1b352db919ad77dc6a9962ce2aece5b96886e03c
rubik <rubik@galadriel.localdomain>
2014-10-08 08:45:36 +0800

    Add check to determine if we should send colorized output

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -6,6 +6,22 @@ def get_color(key):
     use_string = key if isinstance(key, basestring) else str(key)
     return COLORS[use_string.__hash__() % len(COLORS)]
 
+def supports_color():
+    """
+    Taken from https://github.com/django/django/blob/master/django/core/management/color.py
+
+    Returns True if the running system's terminal supports color, and False
+    otherwise.
+    """
+    plat = sys.platform
+    supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
+                                                  'ANSICON' in os.environ)
+    # isatty is not always implemented, #6223.
+    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+    if not supported_platform or not is_a_tty:
+        return False
+    return True
+
 def write_stdout(s):
     sys.stdout.write(s)
     sys.stdout.flush()
@@ -25,11 +41,14 @@ def main():
 def event_handler(event, response):
     line, data = response.split('\n', 1)
     headers = dict([ x.split(':') for x in line.split() ])
-    print '\033[1;%sm%s\033[1;m | \033[1;%sm%s\033[1;m | %s' % (get_color(headers['processname']),
-                                                              headers['processname'],
-                                                              get_color(headers['channel']),
-                                                              headers['channel'],
-                                                              data),
+    if supports_color():
+        print '\033[1;%sm%s\033[1;m | \033[1;%sm%s\033[1;m | %s' % (get_color(headers['processname']),
+                                                                  headers['processname'],
+                                                                  get_color(headers['channel']),
+                                                                  headers['channel'],
+                                                                  data)
+    else:
+        print '%s | %s | %s'%(headers['processname'], headers['channel'], data)
 
 if __name__ == '__main__':
     main()

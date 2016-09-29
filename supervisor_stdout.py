aacda095cbca1e0e6094c02c7c4cce3379fd47e2
Elias Nygren <elias.nygren@upcloud.com>
2016-09-29 15:55:53 +0300

    skip empty lines

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -19,7 +19,7 @@ def main():
 def event_handler(event, response):
     line, data = response.split('\n', 1)
     headers = dict([ x.split(':') for x in line.split() ])
-    lines = data.split('\n')
+    lines = [ l for l in data.split('\n') if l not in ['', '\n'] ]
     prefix = '%s %s | '%(headers['processname'], headers['channel'])
     print '\n'.join([ prefix + l for l in lines ])
 

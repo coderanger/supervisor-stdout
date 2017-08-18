bb39e11b9cc8a3a718f0c579a4e3c742020d09ee
Michael Barrientos <mike@syapse.com>
2017-08-18 14:20:26 -0700

    Add python 3 compatibility, bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'supervisor-stdout',
-    version = '0.1.1',
+    version = '0.2.0',
     py_modules = ['supervisor_stdout'],
 
     author = 'Noah Kantrowitz',
